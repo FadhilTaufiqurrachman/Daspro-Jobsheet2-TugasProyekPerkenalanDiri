089449ab37a44421d16988df7ca6ce6d2cc44641
Fadhil Taufiqurrachman <fadhiltaufiqurrachman08@gmail.com>
2024-09-06 17:58:19 +0700

    Update Perkenalan Febryan

--- PerkenalanFebryan.java
+++ PerkenalanFebryan.java
@@ -4,7 +4,7 @@ public class PerkenalanFebryan {
         System.out.println("---------------------------------------------------------");
         System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
         System.out.println("---------------------------------------------------------");
-        System.out.println("Nama    : Febryan Akhmad Taajuddin bin subkhan");
+        System.out.println("Nama    : Febryan Akhmad Taajuddin");
         System.out.println("NIM     : 244107020180");
         System.out.println("Kelas   : 1E");
         System.out.println("No Absen: 10");
@@ -12,11 +12,5 @@ public class PerkenalanFebryan {
         System.out.println("Alamat  : JL. Untung Suropati Sel 5");
         System.out.println("No HP   : 085840625208");
         System.out.println("Hobi    : Membaca novel");
-
-
-
-        System.out.print("Nama saya Febryan Akhmad Taajuddin");
-        System.out.print("Saya adalah mahasiswa baru Jurusan Teknologi Informasi");
-
     }
 }
